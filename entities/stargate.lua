c3e232d77db662382a9f6adde3dfbcee3119a55d
kellbot <kellbot@gmail.com>
2023-04-07 10:07:15 -0400

    Updated artowrk

--- entities/stargate.lua
+++ entities/stargate.lua
@@ -3,140 +3,100 @@ local stargate =  table.deepcopy(data.raw["land-mine"]["land-mine"])
 local name = 'stargate'
 local localised_name = {name}
 
-stargate.name = name
-stargate.localised_name = localised_name
-stargate.trigger_radius = 1
-stargate.timeout = 5 * 60
-stargate.max_health = 200
-stargate.dying_explosion = nil
-stargate.energy_usage = "25MW"
-stargate.collision_box = {{-2.8, -2.8}, {2.8, 2.8}},
-stargate.selection_box = {{-3, -3}, {3, 3}}
-stargate.drawing_box = {{-3, -3.5}, {3, 3}}
-stargate.se_allow_in_space = false
-stargate.action =
-{
-  type = "direct",
-  action_delivery =
-  {
-    type = "instant",
-    target_effects =
+local stargate = {
+    type = "electric-energy-interface",
+    name = "stargate",
+    collision_box = {{-2, -2}, {2, 2}},
+    selection_box = {{-3, -3}, {3, 3}},
+    drawing_box = {{-3, -3.5}, {3, 3}},
+--  timeout_to_close = 5 * 60    
+    max_health = 5000,
+    corpse = "medium-remnants",
+    flags = {
+        "not-blueprintable",
+        "placeable-neutral",
+        "placeable-player",
+        "player-creation",
+        "not-upgradable"
+    },
+    energy_source = {
+        buffer_capacity = "1GJ",
+        input_flow_limit = "50GW",
+        output_flow_limit = "0kW",
+        type = "electric",
+        usage_priority = "secondary-input"
+      },
+    energy_production = "0KW",
+    energy_usage = "10MW",
+    se_allow_in_space = false,
+    minable = {result = name, mining_time = 3},
+    icon = path.."stargate-icon.png",
+    icon_size = 1000,
+
+    pictures =
     {
-      {
-        type = "create-sticker",
-        sticker = "stargate-sticker",
-        trigger_created_entity = true
-      }
-    }
-  }
-}
-stargate.force_die_on_attack = false
-stargate.trigger_force = "all"
-stargate.order = name
-stargate.picture_safe =
-{
-  filename = path.."stargate-open.png",
-  priority = "medium",
-  width = 500,
-  height = 500,
-  hr_version = {
-    filename = path.."hr-stargate-open.png",
-    priority = "medium",
-    width = 1000,
-    height = 1000,
-    scale = 0.5,
-  }
-}
-stargate.picture_set =
-{
-  filename = path.."stargate-open.png",
-  priority = "medium",
-  width = 500,
-  height = 500,
-  hr_version = {
-    filename = path.."hr-stargate-open.png",
-    priority = "medium",
-    width = 1000,
-    height = 1000,
-    scale = 0.5,
-  }
-}
-stargate.picture_set_enemy =
-{
-  filename = path.."stargate-open.png",
-  priority = "medium",
-  width = 500,
-  height = 500,
-  hr_version = {
-    filename = path.."hr-stargate-open.png",
-    priority = "medium",
-    width = 1000,
-    height = 1000,
-    scale = 0.5,
-  }
-}
-stargate.minable = {result = name, mining_time = 3}
-stargate.flags =
-{
-  --"not-blueprintable",
-  "placeable-neutral",
-  "placeable-player",
-  "player-creation",
-  "not-upgradable"
-}
-stargate.collision_box = {{-1, -1},{1, 1}}
-stargate.selection_box = {{-1, -1},{1, 1}}
-stargate.map_color = {r = 0.5, g = 1, b = 1}
+        north = 
+            {
+            filename = path.."stargate-north.png",
+            priority = "medium",
+            width = 1000,
+            height = 1000,
+            scale = 1,
+            },
+        south = 
+            {
+            filename = path.."stargate-south.png",
+            priority = "medium",
+            width = 1000,
+            height = 1000,
+            scale = 1,
+            },
+        east = 
+            {
+            filename = path.."stargate-east.png",
+            priority = "medium",
+            width = 1000,
+            height = 1000,
+            scale = 1,
+            },
+        west = 
+            {
+            filename = path.."stargate-west.png",
+            priority = "medium",
+            width = 1000,
+            height = 1000,
+            scale = 1,
+        }
+    },
 
-local sticker =
-{
-  type = "sticker",
-  name = "stargate-sticker",
-  --icon = "__base__/graphics/icons/slowdown-sticker.png",
-  flags = {},
-  animation = util.empty_sprite(),
-  duration_in_ticks = 1,
-  --target_movement_modifier = 1
+
 }
 
+local stargate_item = {
+    type = "item",
+    name = name,
+    icon = path.."stargate-icon.png",
+    icon_size = 1000,
+    subgroup = "transport",
+    stack_size = 1,
+    place_result = name
+}
 
-local stargate_item =  table.deepcopy(data.raw.item["land-mine"])
-stargate_item.name = name
-stargate_item.localised_name = localised_name
-stargate_item.place_result = name
-stargate_item.icon = path.."stargate-icon.png"
-stargate_item.icon_size = 500
-stargate_item.icon_mipmaps = 0
-stargate_item.subgroup = "circuit-network"
-
-
--- local fire = require("data/tf_util/tf_fire_util")
-
--- local stargate_explosion = util.copy(data.raw.explosion.explosion)
--- stargate_explosion.name = "stargate-explosion"
--- stargate_explosion.animations = fire.create_fire_pictures({scale = 1, animation_speed = 0.3})
--- stargate_explosion.sound =
--- {
---   filename = path.."stargate-explosion.ogg",
---   volume = 0.45
--- }
-
--- local stargate_explosion_2 = util.copy(stargate_explosion)
--- stargate_explosion_2.name = "stargate-explosion-no-sound"
--- stargate_explosion_2.sound = nil
 
 local recipe = {
   type = "recipe",
   name = name,
   localised_name = localised_name,
   enabled = false,
-  ingredients =
-  {
-    {"steel-plate", 45},
-    {"advanced-circuit", 20},
-    {"battery", 25},
+  ingredients = {
+    { "glass", 200 },
+    { "copper-plate", 200 },
+    { "steel-plate", 200 },
+    { "concrete", 200 },
+    { "battery", 100 },
+    { "processing-unit", 100 },
   },
-  energy_required = 5,
+  energy_required = 10,
   result = name
 }
 
@@ -145,7 +105,6 @@ local technology =
   type = "technology",
   name = name,
   localised_name = localised_name,
-  localised_description = "",
   icon_size = 500,
   icon = path.."stargate-technology.png",
   effects =
@@ -169,27 +128,10 @@ local technology =
   order = "y-a"
 }
 
-local stargate_flying_text = util.copy(data.raw["flying-text"]["tutorial-flying-text"])
-stargate_flying_text.name = "stargate-flying-text"
-
--- local hotkey_name = require"shared".hotkeys.focus_search
--- local hotkey =
--- {
---   type = "custom-input",
---   name = hotkey_name,
---   linked_game_control = "focus-search",
---   key_sequence = "Control + F"
--- }
-
 data:extend
 {
   stargate,
   stargate_item,
---   stargate_explosion,
---   stargate_explosion_2,
   recipe,
-  technology,
-  stargate_flying_text,
---   hotkey,
-  sticker
+  technology
 }
